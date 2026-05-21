450b253e5a020758ff32ab211e4a9679fce6e627
sotom28 <rod.sotod@duocuc.cl>
2026-05-21 19:12:29 -0400

    feat: BACKEND cambio front5 pagina

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.secret_key = os.getenv('SECRET_KEY', 'clave_secreta_por_defecto')
 CORS(app)
 
 # Configuración de la URL del backend API
-BACKEND_URL = os.getenv('BACKEND_URL', 'http://localhost:3000')
+BACKEND_URL = os.getenv('BACKEND_URL', 'http://10.0.138.63:3000')
 
 # Ruta principal - muestra la página de inicio
 @app.route('/')
